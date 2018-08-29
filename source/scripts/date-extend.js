37505ff244b02df49e81e3d1fe23c36dea62c171
Matthew Lancellotti <mareoraft@gmail.com>
2018-08-29 16:19:13 -0400

    switch datejs to npm's version of datejs

--- source/scripts/date-extend.js
+++ source/scripts/date-extend.js
@@ -1,10 +1,28 @@
-// extend the Date object MORE than Date.js does
+/* extend the Date object MORE than Date.js does */
 
-let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-Date.prototype.getWeekdayName = function() {
-    return weekdays[ this.getDay() ]
+////////////////// IMPORTS //////////////////
+require('datejs')
+
+////////////////// GLOBALS //////////////////
+
+/////////////////// MAIN ///////////////////
+function initDateExtend() {
+	let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	Date.prototype.getWeekdayName = function() {
+	    return weekdays[ this.getDay() ]
+	}
+	let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	Date.prototype.getMonthName = function() {
+	    return months[ this.getMonth() ]
+	}
 }
-let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-Date.prototype.getMonthName = function() {
-    return months[ this.getMonth() ]
+
+function getTimestamp() {
+	// get a human friendly timestamp
+	let datetime = new Date()
+	let datetime_string = datetime.toString("yyyy, MMM dd, HH:mm, ss") + " sec"
+	return datetime_string
 }
+
+////////////////// EXPORTS //////////////////
+module.exports = {initDateExtend, getTimestamp}
